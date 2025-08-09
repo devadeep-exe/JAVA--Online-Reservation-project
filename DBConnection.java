4b269cafd2e98165fb713b960863770bd877c4ab
devadeep-exe <devadeep.adak.cse.2022@tint.edu.in>
2025-08-09 19:39:37 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,11 +5,12 @@
     public class DBConnection {
         static final String URL = "jdbc:mysql://localhost:3306/reservation_db";
         static final String USER = "root";
-        static final String PASS = "@Aries02042004";
+        static final String PASS = "PASSWORD";
 
         public static Connection getConnection() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASS);
         }
     }
+
 
 
