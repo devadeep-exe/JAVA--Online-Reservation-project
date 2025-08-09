3b6d7fb214c5b2a5fc8cb79b6c58b3db2fa53d86
devadeep-exe <devadeep.adak.cse.2022@tint.edu.in>
2025-08-09 19:06:24 +0530

    Update Loginsystem.java

--- Loginsystem.java
+++ Loginsystem.java
@@ -1,42 +1,4 @@
-//import java.sql.*;
-//
-//public class Loginsystem {
-//    public static boolean login(String username, String password) {
-//        try (Connection conn = DBConnection.getConnection()) {
-//            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-//            PreparedStatement ps = conn.prepareStatement(sql);
-//            ps.setString(1, username);
-//            ps.setString(2, password);
-//            ResultSet rs = ps.executeQuery();
-//            return rs.next();
-//        } catch (Exception e) {
-//            System.out.println("Login failed: " + e.getMessage());
-//            return false;
-//        }
-//    }
-//}
 
-//-----------------------------------------------------
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//
-//public class Loginsystem {
-//    public static boolean login(String username, String password, Connection conn) {
-//        try {
-//            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-//            PreparedStatement ps = conn.prepareStatement(sql);
-//            ps.setString(1, username);
-//            ps.setString(2, password);
-//            ResultSet rs = ps.executeQuery();
-//            return rs.next(); // true if match found
-//        } catch (Exception e) {
-//            System.out.println("❌ Login failed: " + e.getMessage());
-//            return false;
-//        }
-//    }
-//}
-//-----------------------------------------
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -57,3 +19,4 @@ public class Loginsystem {
     }
 }
 //
+
